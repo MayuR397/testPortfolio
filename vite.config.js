b571b1f231d1b0d8dacd48fba3dc9cf870d5d714
unknown <mayurkhachane2012@gmail.com>
2023-12-02 14:24:09 +0530

    added new

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:/Vijendra-Portfolio.github.io/
+  base:"/testPortfolio/",
+  plugins: [react()]
+
 })
